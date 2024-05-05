632f63fee26f0f7998e459efdd5d86195c738617
daagaard <daniel.aagaard@estudiantat.upc.edu>
2024-05-05 03:57:20 +0200

    aaa

--- brawl/src/App.js
+++ brawl/src/App.js
@@ -9,7 +9,7 @@ function CSVReader() {
     const fetchData = async () => {
       const response = await fetch('/data.csv');
       const text = await response.text();
-      const rows = text.split('\n').map(row => row.split(','));
+      const rows = text.split('\n').map(row => row.split(';'));
       // Assuming the first row contains headers
       const headers = rows[0];
       const rowsData = rows.slice(1);
@@ -30,46 +30,13 @@ function CSVReader() {
       <div style={{ display: 'flex', flexWrap: 'wrap' }}>
         {data.map((row, index) => (
           <div key={index} style={{ margin: '10px' }}>
-              <img src={require('../src/images/'+ row.Nombre +'.png')} alt={row[0]} style={{ width: '200px', height: '200px', objectFit: 'cover' }} />
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-
-
-  return (
-    <div>
-      <h1>CSV Data</h1>
-      <ul>
-        {data.map((row, index) => (
-          <li key={index}>
-            {Object.keys(row).map((key, index) => (
-              <span key={index}>
-                <strong>{key}: </strong> {row[key]}
-                <br />
-              </span>
-            ))}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-
-   return (
-    <div>
-      <h1>Images from CSV</h1>
-      <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-        {data.map((row, index) => (
-          <div key={index} style={{ margin: '10px' }}>
-            <img src={row.image} alt={`Image ${index}`} style={{ width: '200px', height: '200px', objectFit: 'cover' }} />
+              <img className='brawler' title={row.Recomendados} src={require('../src/images/'+ row.Nombre +'.png')} alt={row.Nombre} style={{ width: '200px', height: '200px', objectFit: 'cover'}} />
           </div>
         ))}
       </div>
     </div>
   );
 }
-
 
 function App() {
   return (
@@ -81,12 +48,9 @@ function App() {
        </div>
       </header>
       <body className="App-body">
-        
         <CSVReader></CSVReader>
       </body>
-
     </div>
-
   );
 }
 
