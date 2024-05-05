9040927784323beaec499144a35667c55f94a404
daagaard <daniel.aagaard@estudiantat.upc.edu>
2024-05-05 03:20:36 +0200

    a

--- brawl/src/App.js
+++ brawl/src/App.js
@@ -27,12 +27,42 @@ function CSVReader() {
 
   return (
     <div>
-      <h1>Images and Names from CSV</h1>
       <div style={{ display: 'flex', flexWrap: 'wrap' }}>
         {data.map((row, index) => (
-          <div key={row.name} style={{ margin: '10px', textAlign: 'center' }}>
-            <p>{row.name}</p>
-            <img src={row.image} alt={`Image ${row[name]}`} style={{ width: '200px', height: '200px', objectFit: 'cover' }} />
+          <div key={index} style={{ margin: '10px' }}>
+              <img src={require('../src/images/'+ row.Nombre +'.png')} alt={row[0]} style={{ width: '200px', height: '200px', objectFit: 'cover' }} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+
+
+  return (
+    <div>
+      <h1>CSV Data</h1>
+      <ul>
+        {data.map((row, index) => (
+          <li key={index}>
+            {Object.keys(row).map((key, index) => (
+              <span key={index}>
+                <strong>{key}: </strong> {row[key]}
+                <br />
+              </span>
+            ))}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+
+   return (
+    <div>
+      <h1>Images from CSV</h1>
+      <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+        {data.map((row, index) => (
+          <div key={index} style={{ margin: '10px' }}>
+            <img src={row.image} alt={`Image ${index}`} style={{ width: '200px', height: '200px', objectFit: 'cover' }} />
           </div>
         ))}
       </div>
